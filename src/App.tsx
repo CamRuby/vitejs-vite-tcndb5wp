8b156a7b25f172c164a9bd9127da600691782f8b
CamRuby <camrubysp@gmail.com>
2026-05-25 09:03:17 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { useState, useEffect } from 'react'
+import { supabase } from './supabase'
+import Login from './Login'
+import Dashboard from './Dashboard'
+import ProfesorApp from './pages/ProfesorApp'
+
 export default function App() {
-  return <div>Funciona</div>
+  return <div>Imports OK</div>
 }
